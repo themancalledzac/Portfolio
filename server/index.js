97ee42afbbaa8124ff06d81af5e9a2634d6bfcbd
Zac Edens <edens.zac@gmail.com>
2021-11-02 16:51:23 -0700

    Updated our server file with an index.js boilerplate code

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// here is our graphql schema definition.
+
+const typeDefs = gql`
+  type Image {
+    title: String
+    author: String
+    location: String
+  }
+
+  type Query {
+    images: [Image]
+  }
+`;
+
+// Here is our definition of our data set (i'd assume this is the same as our seed data?)
+
+const images = [
+  {
+    title: "Tara testing",
+    author: "Zechariah Edens",
+    location: "Seattle",
+  },
+  {
+    title: "Tara testing 2",
+    author: "Zechariah Edens",
+    location: "Seattle",
+  },
+];
+
+// Here we define a resolver
+// Our data set is defined as above, but Apollo Server doesn't know that it should USE that data et when it's executing a query. to fix this, we create a resolver
+// Resolvers tell Apollo Server HOW to fetch the data associated with a particular type. Because our 'Photo' array is hardcoded, the corresponding resolver is straightforward.
+
+const resolvers = {
+  Query: {
+    images: () => images,
+  },
+};
+
+// Creat an instance of ApolloServer
+// we've defined our schema, data set, and resolver. now we just need to provide this information to Apollo Server when we initialize it.
+// The ApolloServer constructor requires two parameters: your schema definition and your set of resolvers
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// the 'listen' method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
