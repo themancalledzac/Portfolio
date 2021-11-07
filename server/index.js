02641fc7602620adde9bade678ef500304aa074c
Zac Edens <edens.zac@gmail.com>
2021-11-06 22:08:41 -0700

    updated to include express, mongoose, nodemon, updated apollo-server to the express version. Server now starts

--- server/index.js
+++ server/index.js
@@ -1,22 +1,9 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import express from "express";
+import http from "http";
 import images from "./dummy_images_db.js";
 // here is our graphql schema definition.
-
-const typeDefs = gql`
-  type Image {
-    title: String
-    author: String
-    country: String
-    state: String
-    location: String
-    google_maps: String
-    image_url: String
-  }
-
-  type Query {
-    images: [Image]
-  }
-`;
 
 // Here is our definition of our data set (i'd assume this is the same as our seed data?)
 
@@ -37,19 +24,58 @@ const typeDefs = gql`
 // Our data set is defined as above, but Apollo Server doesn't know that it should USE that data et when it's executing a query. to fix this, we create a resolver
 // Resolvers tell Apollo Server HOW to fetch the data associated with a particular type. Because our 'Photo' array is hardcoded, the corresponding resolver is straightforward.
 
-const resolvers = {
-  Query: {
-    images: () => images,
-  },
-};
-
 // Creat an instance of ApolloServer
 // we've defined our schema, data set, and resolver. now we just need to provide this information to Apollo Server when we initialize it.
 // The ApolloServer constructor requires two parameters: your schema definition and your set of resolvers
 
-const server = new ApolloServer({ typeDefs, resolvers });
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
-// the 'listen' method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+// const server = new ApolloServer({ typeDefs, resolvers });
+// const app = express();
+// server.applyMiddleware({ app });
+
+// // the 'listen' method launches a web server.
+// app.listen({ port: 4000 }, () => {
+//   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+// });
+
+async function startApolloServer() {
+  const typeDefs = gql`
+    type Image {
+      title: String
+      author: String
+      country: String
+      state: String
+      location: String
+      google_maps: String
+      image_url: String
+    }
+
+    type Query {
+      images: [Image]
+    }
+  `;
+  const resolvers = {
+    Query: {
+      images: () => images,
+    },
+  };
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+startApolloServer();
