ce832cb7834e53a4919982f16eeae5b626cb946f
Zac Edens <edens.zac@gmail.com>
2021-11-12 14:53:16 -0800

    updated our custom nextjs _app.js file and _document.js files

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,26 @@
 import "../styles/globals.css";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 // import client from "../apollo-client";
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:3000/graphql" }),
+]);
 
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
@@ -8,10 +28,15 @@ const client = new ApolloClient({
 });
 
 // In our _app.js file, we are setting up our 'client side'rendering.
+// the Component prop is the active 'page', so whenever you navigate between routes, 'Component' will change to the new 'page'. therefore, any props you send to 'Component' will be received by the 'page'.
+// 'pageProps' is an object with the initial props that were preloaded for your page by one of our data fetching methods, otherwise it's an empty object.
+
 
 function App({ Component, pageProps }) {
   return (
+    // we wrap our entire project in our ApolloProvider, which lets our entire application access our graphql api
     <ApolloProvider client={client}>
+    
       <Component {...pageProps} />
     </ApolloProvider>
   );
